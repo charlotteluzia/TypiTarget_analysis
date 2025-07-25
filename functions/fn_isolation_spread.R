5bac1ffc120004c51494a2fe350fc9d8f4585316
charlotteluzia <atzert.charlotte@gmail.com>
2025-07-25 11:23:53 +0200

    changes to complete data analysis

--- functions/fn_isolation_spread.R
+++ functions/fn_isolation_spread.R
@@ -6,16 +6,13 @@ fn_isolation_spread <- function(all_data, vars){
     filter(iso_spread == 1) %>%
     group_by(participant, typicality, category, iso_spread) %>%
     summarize(
-      mem_hitr_iso      = (sum(mem_perform == "hit" , na.rm = TRUE) + 0.5) / (sum(cond == "old", na.rm = TRUE) + 1),
-      mem_falr_iso      = (sum(mem_perform == "fa" , na.rm = TRUE) + 0.5)  / (sum(cond == "new", na.rm = TRUE) + 1),
+      mem_hitr      = (sum(mem_perform == "hit" , na.rm = TRUE) + 0.5) / (sum(cond == "old", na.rm = TRUE) + 1),
+      mem_falr      = (sum(mem_perform == "fa" , na.rm = TRUE) + 0.5)  / (sum(cond == "new", na.rm = TRUE) + 1),
       .groups = 'drop' # Drop the automatic grouping to avoid issues later
     ) %>%
     mutate(
-      # mem_dprime_iso      = qnorm(mem_hitr_iso) - qnorm(mem_falr_iso),
-      # mem_crit_iso        = -0.5 * (qnorm(mem_hitr_iso) + qnorm(mem_falr_iso))
-      mem_dprime      = qnorm(mem_hitr_iso) - qnorm(mem_falr_iso),
-      mem_crit        = -0.5 * (qnorm(mem_hitr_iso) + qnorm(mem_falr_iso)),
-      # iso             = 1
+      mem_dprime      = qnorm(mem_hitr) - qnorm(mem_falr),
+      mem_crit        = -0.5 * (qnorm(mem_hitr) + qnorm(mem_falr)),
     )
   
   summary_no_iso_spread <- all_data %>%
@@ -24,16 +21,13 @@ fn_isolation_spread <- function(all_data, vars){
     filter(category %in% vars$categories) %>%
     group_by(participant, typicality, category, iso_spread) %>%
     summarize(
-      mem_hitr_no_iso      = (sum(mem_perform == "hit" , na.rm = TRUE) + 0.5) / (sum(cond == "old", na.rm = TRUE) + 1),
-      mem_falr_no_iso      = (sum(mem_perform == "fa" , na.rm = TRUE) + 0.5)  / (sum(cond == "new", na.rm = TRUE) + 1),
+      mem_hitr      = (sum(mem_perform == "hit" , na.rm = TRUE) + 0.5) / (sum(cond == "old", na.rm = TRUE) + 1),
+      mem_falr      = (sum(mem_perform == "fa" , na.rm = TRUE) + 0.5)  / (sum(cond == "new", na.rm = TRUE) + 1),
       .groups = 'drop' # Drop the automatic grouping to avoid issues later
     ) %>%
     mutate(
-      # mem_dprime_no_iso      = qnorm(mem_hitr_no_iso) - qnorm(mem_falr_no_iso),
-      # mem_crit_no_iso        = -0.5 * (qnorm(mem_hitr_no_iso) + qnorm(mem_falr_no_iso))
-      mem_dprime      = qnorm(mem_hitr_no_iso) - qnorm(mem_falr_no_iso),
-      mem_crit        = -0.5 * (qnorm(mem_hitr_no_iso) + qnorm(mem_falr_no_iso)),
-      # iso             = 0
+      mem_dprime      = qnorm(mem_hitr) - qnorm(mem_falr),
+      mem_crit        = -0.5 * (qnorm(mem_hitr) + qnorm(mem_falr)),
     )
   
   summary_iso_spread_complete <- summary_iso_spread %>%
